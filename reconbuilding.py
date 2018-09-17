5e5c01482fc059ff26d89810b56969b6c65d2b54
root <root@localhost.localdomain>
2018-09-16 20:31:45 -0400

    Works still a lot to fix

--- reconbuilding.py
+++ reconbuilding.py
@@ -15,16 +15,16 @@ if len(sys.argv) != 2:
     sys.exit(0) 
 
 domain = sys.argv[1]
-amassoutput = domain + "_amass.txt"
-amassvm = "amass -freq 480 -d " + domain + " -o " + amassoutput
+
 
 #Define amass
 def amassrun():
+    amassoutput = domain + "_amass.txt"
+    amassvm = "amass -freq 480 -d " + domain + " -o " + amassoutput
     os.system(amassvm) 
 
 #Define Dig
-def digrun():
-    
+def digrun():    
     rundig = "dig -f " + amassoutput + " > " + domain + "_dig.txt"
     os.system(rundig)
 
@@ -35,22 +35,37 @@ def formatdigips():
 
 #Define NMAP PORTS 80, 8080, and 443
 def nmapwebrun():
-    nmap_fullcommand = "nmap -v -p 80,8080,443 -oG " + domain + "_webonly_nmap -iL " + domain + "_digformated.txt"
+    nmap_fullcommand = "nmap -v -p 80,8080,443 -oA " + domain + "_webonly_nmap -iL " + domain + "_digformated.txt"
     os.system(nmap_fullcommand)
        
 #Define NMAP
 def nmapfullrun():
-    nmap_fullcommand = "nmap -v -p 1-65535 -oG " + domain + "_allport_nmap -iL " + domain + "_digformated.txt"
+    nmap_fullcommand = "nmap -v -p 1-65535 -oA " + domain + "_allport_nmap -iL " + domain + "_digformated.txt"
     os.system(nmap_fullcommand)
 
-#Define Dirb
+#Define which ports to dirb
+def dirbports():
+    dirbhttpcommand = "cat " + domain + "_webonly_nmap.nmap | grep open | grep -v https | cut -d '/' -f1 > " + domain + "_open_http_ports.txt"
+    dirbhttpscommand = "cat " + domain + "_webonly_nmap.nmap | grep open | grep -v http | cut -d '/' -f1 > " + domain + "_open_https_ports.txt"
+    os.system(dirbhttpcommand)
+    os.system(dirbhttpscommand)   
+
+#Define Dirb run
 def dirbrun():
-    #dirbcommand = dirb ""
-    print "dirbrun"
+    dirbhttpports =  domain + "_open_http_ports.txt" 
+    dirbhttpsports = domain + "_open_https_ports.txt"
+    fh = open(dirbhttpports)
+    for line in fh:
+        dirbruncommand = "dirb http://" + domain + ":" + line + ">>" + domain + "_all_dirb_output.txt"
+        print dirbruncommand
+        #os.system(dirbruncommand)
+    fh.close
+
 
 #Define EyeWitness
 def eyewitenessrun():
-    print "eyewitenessrun"
+    eyewitnesscommand = "python /root/tools/EyeWitness/EyeWitness.py --web --no-prompt -x " + domain + "_webonly_nmap.xml"
+    os.system(eyewitnesscommand)
 
 #define Nikto
 def niktorun():
@@ -59,9 +74,13 @@ def niktorun():
 #define spider
 
 
-#execute code
-amassrun()
-digrun()
-formatdigips()
-nmapwebrun()
+#execute code below
+
+#amassrun()
+#digrun()
+#formatdigips()
+#nmapwebrun()
 #nmaprun()
+#dirbports()
+#dirbrun() # not working
+eyewitenessrun() # working lets work on output
