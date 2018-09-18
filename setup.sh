1454ef2c446c02edb8154340cbdc90f0556bf14b
root <root@localhost.localdomain>
2018-09-18 08:34:48 -0400

    updates

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@ sudo apt-get install python-pip python-dev build-essential -y
 pip install --upgrade pip -y
 git clone https://github.com/weirdatfirst/EyeWitness.git
 git clone https://github.com/weirdatfirst/Sublist3r.git
+echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" > /etc/apt/sources.list.d/kali.list
+apt-get get update
 sudo sh EyeWitness/setup/setup.sh -y
 sudo pip install -r Sublist3r/requirements.txt
+sudo apt-get install -y nmap dirb nikto
 
